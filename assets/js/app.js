15e31f8d7c22518b5012c707d79384944f699e6e
Derrik Dennis <derrik@austin.rr.com>
2020-09-03 14:18:55 -0500

    Finalizing basic challenge.  No bonus.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
+// Create svg Area for Chart
 var svgWidth = 960;
 var svgHeight = 660;
 var padding = 20;
 
+//Create Chart Margins to help axes and placement of chart
 var chartMargin = {
     top: 30,
     right: 30,
@@ -9,17 +11,21 @@ var chartMargin = {
     left: 30
 };
 
-var strokecolor = "#9999ff";
+//This will help with the left axis
+var AxisLeft = chartMargin.left + 20;
 
+
+//This will finalize the area that is going to be used within the svg area of the chart
 var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
 var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
+//Creating the svg which will be a scatterplot
 var svg = d3.select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
-
+//Bringing in the state by state data
 d3.csv("./assets/data/data.csv").then(function (statedata) {
 
     statedata.forEach(function (data) {
@@ -43,20 +49,22 @@ d3.csv("./assets/data/data.csv").then(function (statedata) {
         data.smokesHigh = +data.smokesHigh;
     });
 
+    //Creating scales so that everything moves with the scale both on the x and y axis
     var xScale = d3.scaleLinear()
         .domain([d3.min(statedata, function (d) {
-            return d.poverty;
+            return d.poverty - 1;
         }), d3.max(statedata, function (d) {
             return d.poverty;
         })])
-        .range([10, chartWidth + 20]);
+        .range([AxisLeft, svgWidth - chartMargin.right]);
 
     var yScale = d3.scaleLinear()
         .domain([0, d3.max(statedata, function (d) {
             return d.smokes;
         })])
-        .range([chartHeight, 10]);
+        .range([svgHeight - chartMargin.top, chartMargin.bottom]);
 
+    //Creatng our circles.  I chose to look at smokers by poverty level
     var circles = svg.selectAll("circle")
         .data(statedata)
         .enter()
@@ -67,11 +75,10 @@ d3.csv("./assets/data/data.csv").then(function (statedata) {
         .attr("cy", function (d) {
             return yScale(d.smokes);
         })
-        .attr("r", 10)
-        .attr("fill", "white")
-        .attr("stroke", strokecolor)
-        .attr("stroke-width", "3px");
+        .attr("r", 14)
+        .attr("class", "stateCircle");
 
+    //Add the state abbreviations as labels
     var text = svg.selectAll("text")
         .data(statedata)
         .enter()
@@ -80,28 +87,42 @@ d3.csv("./assets/data/data.csv").then(function (statedata) {
             return d.abbr;
         })
         .attr("x", function (d) {
-            return xScale(d.poverty) - 6;
+            return xScale(d.poverty);
         })
         .attr("y", function (d) {
-            return yScale(d.smokes) + 5;
+            return yScale(d.smokes) + 4;
         })
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "11px")
-        .attr("fill", "black");
+        .attr("class", "stateText");
 
+    //Creating both the x and y axes        
     var xAxis = d3.axisTop(xScale);
 
     svg.append("g")
         .attr("class", "axis")
-        .attr("transform", "translate(0," + (chartHeight) + ")")
+        .attr("transform", "translate(0," + (svgHeight - chartMargin.bottom) + ")")
         .call(xAxis);
 
     var yAxis = d3.axisLeft(yScale);
 
-    svgAppend("g")
+    svg.append("g")
         .attr("class", "axis")
+        .attr("transform", "translate(" + AxisLeft + ",0)")
         .call(yAxis);
 
-    console.log(data);
+    svg.append("text")
+        .attr("class", "aText")
+        .attr("text-anchor", "end")
+        .attr("x", svgWidth / 2)
+        .attr("y", (svgHeight - 10))
+        .text("Poverty Level by State (%)");
+
+    svg.append("text")
+        .attr("class", "aText")
+        .attr("text-anchor", "end")
+        .attr("x", (svgHeight / -2))
+        .attr("y", (chartMargin.left - 10))
+        //        .attr("dy", ".75em")
+        .attr("transform", "rotate(-90)")
+        .text("Smokers by State (%)");
 
 });
